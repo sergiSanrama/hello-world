be1e303f40cf08b5d29f18e48916e2b5944ce899
sergiSanramaT <sergisanramat@gmail.com>
2018-07-03 12:49:39 +0200

    alertInstructions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 function showMessage(){
     alert("Hello, World! Hola, Mundo! Hallo, Welt!");
 }
-
+function showAlertInstructions(){
+    alert("Hello, World! Hola, Mundo! Hallo, Welt!");
+}
 console.log("carga bien");
